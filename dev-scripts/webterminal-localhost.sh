c51e8a2b039651f611488c212ed1c593ad4ced61
Sergii Leshchenko <sleshche@redhat.com>
2021-02-08 14:58:33 +0200

    Actulize script to run web-terminal on localhost

--- dev-scripts/webterminal-localhost.sh
+++ dev-scripts/webterminal-localhost.sh
@@ -6,32 +6,42 @@
 set -e
 
 ## One time setup:
-# sudo ln -s ~/kube-in-cluster-emulation/ca.crt /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-# sudo ln -s ~/kube-in-cluster-emulation/token /var/run/secrets/kubernetes.io/serviceaccount/token
+# sudo mkdir -p /var/run/secrets/kubernetes.io/serviceaccount/
+# sudo ln -s /tmp/kube-in-cluster-emulation /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+# sudo ln -s /tmp/kube-in-cluster-emulation /var/run/secrets/kubernetes.io/serviceaccount/token
 
+mkdir -p /tmp/kube-in-cluster-emulation
 # Set up
 function setup() {
   SA_SECRET=$(oc get sa console -n openshift-console -o yaml | yq -r '.secrets[].name' | grep "console-token-.*")
-  oc get secret ${SA_SECRET} -n openshift-console -o json | jq -r '.data["ca.crt"]' | base64 -d > ~/kube-in-cluster-emulation/ca.crt
-  oc get secret ${SA_SECRET} -n openshift-console -o json | jq -r '.data["token"]'  | base64 -d > ~/kube-in-cluster-emulation/token
+  oc get secret ${SA_SECRET} -n openshift-console -o json | jq -r '.data["ca.crt"]' | base64 -d > /tmp/kube-in-cluster-emulation/ca.crt
+  oc get secret ${SA_SECRET} -n openshift-console -o json | jq -r '.data["token"]'  | base64 -d > /tmp/kube-in-cluster-emulation/token
 }
 
 function update_backend() {
+  echo "Patching backend"
   sed -i.bak 's/if terminalHost.Scheme != "https"/if false/' ./pkg/terminal/proxy.go
   rm ./pkg/terminal/proxy.go.bak
+  git --no-pager diff ./pkg/terminal/proxy.go
+  echo "Compiling patched backend"
   ./build-backend.sh
+  echo "Reverting patch"
   sed -i.bak 's/if false/if terminalHost.Scheme != "https"/' ./pkg/terminal/proxy.go
   rm ./pkg/terminal/proxy.go.bak
 }
 
 function update_frontend() {
-  sed -i.bak -e "s|routingClass: 'openshift-terminal'|routingClass: 'basic'|" \
-             -e "s|id: 'che-incubator/command-line-terminal/4.5.0'|id: 'che-incubator/command-line-terminal-dev/4.5.0'|" \
+  echo "Patching frontend"
+  sed -i.bak -e "s|routingClass: 'web-terminal'|routingClass: 'basic'|" \
+             -e "s|id: 'redhat-developer/web-terminal/4.5.0'|id: 'redhat-developer/web-terminal-dev/4.5.0'|" \
       ./frontend/packages/console-app/src/components/cloud-shell/cloud-shell-utils.ts
   rm ./frontend/packages/console-app/src/components/cloud-shell/cloud-shell-utils.ts.bak
+  git --no-pager diff ./frontend/packages/console-app/src/components/cloud-shell/cloud-shell-utils.ts
+  echo "Compiling patched frontend"
   ./build-frontend.sh
-  sed -i.bak -e "s|routingClass: 'basic'|routingClass: 'openshift-terminal'|" \
-             -e "s|id: 'che-incubator/command-line-terminal-dev/4.5.0'|id: 'che-incubator/command-line-terminal/4.5.0'|" \
+  echo "Reverting patch"
+  sed -i.bak -e "s|routingClass: 'basic'|routingClass: 'web-terminal'|" \
+             -e "s|id: 'redhat-developer/web-terminal-dev/4.5.0'|id: 'redhat-developer/web-terminal/4.5.0'|" \
       ./frontend/packages/console-app/src/components/cloud-shell/cloud-shell-utils.ts
   rm ./frontend/packages/console-app/src/components/cloud-shell/cloud-shell-utils.ts.bak
 }
