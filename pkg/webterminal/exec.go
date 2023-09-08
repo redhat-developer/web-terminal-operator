db63742022fcc328d88a489c2a6fdaa43ef32159
Angel Misevski <amisevsk@redhat.com>
2023-09-08 14:05:05 -0400

    Update images in templates even if state is unmanaged
    If a template is in an unmanaged state but still uses a default image
    (i.e. one from the same image repository as the new one), update only
    the image in the unmanaged template to ensure CVE fixes still get
    applied to templates that change e.g. default resource limits/requests.
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- pkg/webterminal/exec.go
+++ pkg/webterminal/exec.go
@@ -15,6 +15,7 @@ package webterminal
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	dw "github.com/devfile/api/v2/pkg/apis/workspaces/v1alpha2"
 	"github.com/devfile/api/v2/pkg/attributes"
@@ -41,18 +42,64 @@ func syncExecTemplate(ctx context.Context, client crclient.Client, namespace str
 		return client.Create(ctx, specDWT)
 	}
 
+	var updatedDWT *dw.DevWorkspaceTemplate
 	if clusterDWT.Annotations != nil && clusterDWT.Annotations[config.UnmanagedStateAnnotation] == "true" {
-		log.Info("Found unmanaged template for Web Terminal Exec; skipping.")
-		return nil
+		log.Info("Found unmanaged template for Web Terminal Exec.")
+		updatedDWT = handleUnmanagedExecState(specDWT, clusterDWT)
+	} else {
+		specDWT.ResourceVersion = clusterDWT.ResourceVersion
+		updatedDWT = specDWT
 	}
 
-	specDWT.ResourceVersion = clusterDWT.ResourceVersion
-	err = client.Update(ctx, specDWT)
+	err = client.Update(ctx, updatedDWT)
 	if err != nil {
 		return fmt.Errorf("error updating Web Terminal Exec template: %w", err)
 	}
 	log.Info("Web Terminal Exec template updated.")
 	return nil
+}
+
+func handleUnmanagedExecState(spec, cluster *dw.DevWorkspaceTemplate) *dw.DevWorkspaceTemplate {
+	result := cluster.DeepCopy()
+
+	// Even though the template is marked as unmanaged, still try to update the image used if the current
+	// image is a default image to ensure CVE fixes are propagated
+	specImage, err := config.GetDefaultExecImage()
+	if err != nil {
+		// Should never happen, since getSpecExecTemplate depends on the same function
+		log.Info("warning: Could not get default Web Terminal Exec container image")
+		return cluster
+	}
+	var clusterImage string
+	for _, component := range cluster.Spec.Components {
+		if component.Name == config.ExecTemplateName && component.Container != nil {
+			clusterImage = component.Container.Image
+		}
+	}
+	if specImage == clusterImage {
+		// No update needed
+		return cluster
+	}
+
+	if clusterImage == "" {
+		// The cluster template is potentially _very_ different from default; do the safe thing and make no changes
+		return cluster
+	}
+	// Strip digest or tag from images
+	specRepo := strings.Split(specImage, `@`)[0]
+	specRepo = strings.Split(specRepo, `:`)[0]
+	clusterRepo := strings.Split(clusterImage, `@`)[0]
+	clusterRepo = strings.Split(clusterRepo, `:`)[0]
+	if specRepo == clusterRepo {
+		log.Info(fmt.Sprintf("Found default image %s in Web Terminal Exec template. Updating image to %s", clusterImage, specImage))
+		for idx, component := range result.Spec.Components {
+			if component.Name == config.ExecTemplateName {
+				result.Spec.Components[idx].Container.Image = specImage
+			}
+		}
+	}
+
+	return result
 }
 
 func getSpecExecTemplate(namespace string) (*dw.DevWorkspaceTemplate, error) {
